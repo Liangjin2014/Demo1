a94a5f8ec1afce0b7758482fd12d35c195933484
yun <970720620@qq.com>
2018-11-19 21:12:29 +0800

    get data from django

--- app/src/main/java/com/example/yun/demo/MainActivity.java
+++ app/src/main/java/com/example/yun/demo/MainActivity.java
@@ -5,6 +5,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.gson.Gson;
 
@@ -30,9 +31,9 @@ import okhttp3.Response;
  *
  * */
 
-public class MainActivity extends BaseActivity{
+public class MainActivity extends BaseActivity {
 
-    private final String url = "http://192.168.0.102:9000/jsontest/";
+    private final String url = "http://192.168.0.103:9000/jsontest/";
     @BindView(R.id.et_username)
     EditText etUsername;
     @BindView(R.id.et_password)
@@ -40,36 +41,13 @@ public class MainActivity extends BaseActivity{
     @BindView(R.id.button)
     Button button;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
         EventBus.getDefault().register(this);
-    }
-
-    //异步向服务器发送GET请求来获取数据
-    private void getDataAsync() {
-        OkHttpClient okHttpClient = new OkHttpClient();
-        //2.创建Request对象，设置一个url地址,设置请求方式。
-        Request request = new Request.Builder().url(url).method("GET", null).build();
-        //3.创建一个call对象,参数就是Request请求对象
-        Call call = okHttpClient.newCall(request);
-        //4.请求加入调度，重写回调方法
-        call.enqueue(new Callback() {
-            //请求失败执行的方法
-            @Override
-            public void onFailure(Call call, IOException e) {
-                Log.e("ERROR", "获取数据失败！");
-            }
-
-            //请求成功执行的方法
-            @Override
-            public void onResponse(Call call, Response response) throws IOException {
-                String name = parseData(response.body().string());
-                EventBus.getDefault().post(name);
-
-            }
-        });
     }
 
     //解析数据
@@ -88,17 +66,37 @@ public class MainActivity extends BaseActivity{
     //EventBus接收消息的方法
     @Subscribe(threadMode = ThreadMode.MAIN, sticky = true)
     public void onEvent(String name) {
-        //    textView.setText(name);
+        Toast.makeText(this, name, Toast.LENGTH_SHORT).show();
     }
 
     @OnClick({R.id.button})
-    public void onViewOnClicked(View view)
-    {
-        switch (view.getId())
-        {
-            case R.id.button:
-                Log.e("BUTTON", "按钮被点击了！");
-        }
+    public void onViewOnClicked(View view) {
+       getDataAsync();
+    }
+
+    //异步向服务器发送GET请求来获取数据
+    private void getDataAsync() {
+        OkHttpClient okHttpClient = new OkHttpClient();
+        //2.创建Request对象，设置一个url地址,设置请求方式。
+        Request request = new Request.Builder().url(url).method("GET", null).build();
+        //3.创建一个call对象,参数就是Request请求对象
+        Call call = okHttpClient.newCall(request);
+        //4.请求加入调度，重写回调方法
+        call.enqueue(new Callback() {
+            //请求失败执行的方法
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Log.e("ERROR", e.toString());
+            }
+
+            //请求成功执行的方法
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String name = parseData(response.body().string());
+                EventBus.getDefault().post(name);
+
+            }
+        });
     }
 
 }
