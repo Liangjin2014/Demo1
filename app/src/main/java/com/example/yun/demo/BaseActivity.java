8d98fbefc690af9fc1cc9f3b76775b945460922e
yun <970720620@qq.com>
2018-11-19 21:13:54 +0800

    delete onCreate

--- app/src/main/java/com/example/yun/demo/BaseActivity.java
+++ app/src/main/java/com/example/yun/demo/BaseActivity.java
@@ -11,9 +11,6 @@ import butterknife.ButterKnife;
 
 public class BaseActivity extends Activity {
 
-    @BindView(R.id.text)
-    TextView text;
-
     @Override
     public void setContentView(View view) {
         setContentView(R.layout.base_activity);
@@ -33,12 +30,6 @@ public class BaseActivity extends Activity {
         ButterKnife.bind(this);
     }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        // TODO: add setContentView(...) invocation
-        ButterKnife.bind(this);
-    }
 }
 
 
