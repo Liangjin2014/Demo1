75abe634d94b50b19dd0b74932c32956593f5aac
yun <970720620@qq.com>
2018-11-19 23:30:00 +0800

    post data to server

--- app/src/main/java/com/example/yun/demo/MainActivity.java
+++ app/src/main/java/com/example/yun/demo/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.yun.demo;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -14,12 +15,14 @@ import org.greenrobot.eventbus.Subscribe;
 import org.greenrobot.eventbus.ThreadMode;
 
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import okhttp3.Call;
 import okhttp3.Callback;
+import okhttp3.FormBody;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -71,7 +74,17 @@ public class MainActivity extends BaseActivity {
 
     @OnClick({R.id.button})
     public void onViewOnClicked(View view) {
-       getDataAsync();
+//       getDataAsync();
+        String username = etUsername.getText().toString();
+        String password = etPassword.getText().toString();
+        if(!TextUtils.isEmpty(username.trim()) && !TextUtils.isEmpty(password.trim()))
+        {
+            postDataSync(username, password);
+        }
+        else
+        {
+            Toast.makeText(this,"姓名或密码不能为空哦！", Toast.LENGTH_LONG).show();
+        }
     }
 
     //异步向服务器发送GET请求来获取数据
@@ -99,4 +112,43 @@ public class MainActivity extends BaseActivity {
         });
     }
 
+   public void postDataSync(String key, String value)
+   {
+       OkHttpClient okHttpClient  = new OkHttpClient.Builder()
+               .connectTimeout(10, TimeUnit.SECONDS)
+               .writeTimeout(10,TimeUnit.SECONDS)
+               .readTimeout(20, TimeUnit.SECONDS)
+               .build();
+
+       //post方式提交的数据
+       FormBody formBody = new FormBody.Builder()
+               .add(key, value)
+               .build();
+
+       final Request request = new Request.Builder()
+               .header()
+               .url(url)//请求的url
+               .post(formBody)
+               .build();
+
+
+       //创建/Call
+       Call call = okHttpClient.newCall(request);
+       //加入队列 异步操作
+       call.enqueue(new Callback() {
+           //请求错误回调方法
+           @Override
+           public void onFailure(Call call, IOException e) {
+               Log.e("ERROR", "链接失败！");
+           }
+
+           @Override
+           public void onResponse(Call call, Response response) throws IOException {
+
+    //               EventBus.getDefault().post(response.body().string());
+                   Log.e("ERROR", response.body().string());
+           }
+       });
+
+   }
 }
